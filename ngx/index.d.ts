9eec2c83b869b5a7b68d2f0fd488c3f77ae360cb
李晨 <lichen@facc.com.cn>
2020-12-06 23:05:33 +0800

    1

--- ngx/index.d.ts
+++ ngx/index.d.ts
@@ -1,42 +1,26 @@
 import { IonicNativePlugin } from '@ionic-native/core';
 /**
- * @name weichat-pay
+ * @name We Chat Pay
  * @description
  * This plugin does something
  *
  * @usage
  * ```typescript
- * import { weichat-pay } from '@ionic-native/weichat-pay';
+ * import { WeChatPay } from '@ionic-native/we-chat-pay';
  *
  *
- * constructor(private weichat-pay: weichat-pay) { }
+ * constructor(private weChatPay: WeChatPay) { }
  *
  * ...
  *
  *
- * this.weichat-pay.functionName('Hello', 123)
+ * this.weChatPay.functionName('Hello', 123)
  *   .then((res: any) => console.log(res))
  *   .catch((error: any) => console.error(error));
  *
  * ```
  */
 export declare class WeChatPay extends IonicNativePlugin {
-    /**
-     * Send a payment request
-     *
-     * @link https://pay.weixin.qq.com/wiki/doc/api/app.php?chapter=9_1
-     * @example
-     * <code>
-     * var params = {
-     *     appid:'appid'
-     *     mch_id: '10000100', // merchant id
-     *     prepay_id: 'wx201411101639507cbf6ffd8b0779950874', // prepay id returned from server
-     *     nonce: '1add1a30ac87aa2db72f57a2375d8fec', // nonce string returned from server
-     *     timestamp: '1439531364', // timestamp
-     *     sign: '0CB01533B8C1EF103065174F50BCA001', // signed string
-     * };
-     * </code>
-     */
-    sendPaymentRequest(params: any): Promise<any>;
+    sendPaymentRequest(arg0: any): Promise<any>;
     auth(scope: any, state: any): Promise<any>;
 }
